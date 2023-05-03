47310011f9c2051b7b35d0b84b649c10b7d1e210
Giselle Santos <gisellesf@dcomp.ufs.br>
2023-05-02 23:11:32 -0300

    002-configuração da tela de login e pag 404

---
+++ src/pages/404.js
@@ -0,0 +1,57 @@
+// ** Next Import
+import Link from 'next/link'
+
+// ** MUI Components
+import Button from '@mui/material/Button'
+import { styled } from '@mui/material/styles'
+import Typography from '@mui/material/Typography'
+import Box from '@mui/material/Box'
+
+// ** Layout Import
+// import BlankLayout from 'src/@core/layouts/BlankLayout'
+
+// ** Styled Components
+const BoxWrapper = styled(Box)(({ theme }) => ({
+  [theme.breakpoints.down('md')]: {
+    width: '90vw'
+  }
+}))
+
+const Img = styled('img')(({ theme }) => ({
+  marginBottom: theme.spacing(10),
+  [theme.breakpoints.down('lg')]: {
+    height: 450,
+    marginTop: theme.spacing(10)
+  },
+  [theme.breakpoints.down('md')]: {
+    height: 400
+  },
+  [theme.breakpoints.up('lg')]: {
+    marginTop: theme.spacing(13)
+  }
+}))
+
+export default function Error404() {
+  return (
+    <Box className='content-center'>
+      <Box sx={{ p: 5, display: 'flex', flexDirection: 'column', alignItems: 'center', textAlign: 'center' }}>
+        <BoxWrapper>
+          <Typography variant='h1'>404</Typography>
+          <Typography variant='h5' sx={{ mb: 1, fontSize: '1.5rem !important' }}>
+            Page Not Found ⚠️
+          </Typography>
+          <Typography variant='body2'>Não foi possível encontrar a página que você está procurando.</Typography>
+        </BoxWrapper>
+        <Img height='487' alt='error-illustration' src='/images/pages/404.png' />
+        <Link passHref href='/'>
+          <Button variant='contained' sx={{ px: 5.5 }}>
+            Vá para a página inicial
+          </Button>
+        </Link>
+      </Box>
+    </Box>
+  )
+}
+
+
+
